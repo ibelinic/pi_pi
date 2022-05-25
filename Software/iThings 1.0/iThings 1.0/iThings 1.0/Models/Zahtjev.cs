b554152fbc9f1a37a3867bd6c631f4fb37d7943b
Ivana Belinić <ibelinic20@foi.hr>
2022-05-25 03:13:49 +0100

    Nastavak

---
+++ Software/iThings 1.0/iThings 1.0/iThings 1.0/Models/Zahtjev.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iThings_1._0.Models
+{
+    internal class Zahtjev
+    {
+        public int Id { get; set; }
+
+        public string Podnositelj { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime DatumPodnosenja { get; set; }
+
+        public string SifraOpreme { get; set; }
+
+        public string NazivOpreme { get; set; }
+
+        public int KolicinaOpreme { get; set; }
+    }
+}
